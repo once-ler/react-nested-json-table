61a1bc4b56fd720c801ab22d7896343c5a807d52
once-ler <htao@eztier.com>
2016-07-20 13:19:33 -0400

    Fix gitignore that hid source code files

---
+++ src/lib/NestedJsonTable.js
@@ -0,0 +1,62 @@
+import './styles.css';
+import React, { Component, PropTypes } from 'react';
+import JsonNode from './JsonNode';
+import Table from './Table';
+
+export default class NestedJsonTable extends Table {
+  
+  static propTypes = {
+    children: PropTypes.any.isRequired
+  }
+
+  constructor(props) {
+    super(props);    
+  }
+
+  render() {
+    let { children, parentKey } = this.props;
+    
+    children = this.ensureChildIsArray(children);
+    this.ensureChildIsObject(children);
+    
+    let header = [];
+    let rows = [];
+
+    children.forEach((child, i) => {
+      for (const key in child) {
+        const n = Math.random();
+        const obj = child[key];
+        const childKey = `${parentKey}/${i}/${key}`;
+
+        header.push(<th key={Math.random()}>{key}</th>);
+        
+        let o = [];
+        if (this.isObject(obj)) {
+          o = this.makeArray(obj);
+        }
+        if (this.isArray(obj) && obj.length > 0) {
+          const modifiedArray = this.makeArrayForEach(obj);
+          rows.push(
+            <td key={n}>
+              <JsonNode path={childKey} children={modifiedArray} />            
+            </td>);
+        } else {
+          if (o.length > 0) {
+            rows.push(
+              <td key={n}>
+                <JsonNode path={childKey} children={o} />            
+              </td>);
+          } else {
+            rows.push(<td key={n}>{obj.toString()}</td>);
+          }
+        }
+      }
+    });
+
+    return (
+      <table><thead><tr>{header}</tr></thead>
+        <tbody><tr>{rows}</tr></tbody>
+      </table>
+    );
+  } 
+}
---
+++ src/lib/Table.js
@@ -0,0 +1,131 @@
+import React, { Component, PropTypes } from 'react';
+import JsonNode from './JsonNode';
+
+/*
+ *  @description Child node table creator
+ */
+export default class Table extends Component {
+
+  static propTypes = {
+    children: PropTypes.array.isRequired,
+    parentKey: PropTypes.string
+  }
+
+  static defaultProps = {
+    parentKey: 'root'
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  /**
+    * @reference jQuery.makeArray
+  **/
+  makeArray(arr, results) {
+    var ret = results || [];
+
+    if (arr != null) {
+      if (this.isArray(Object(arr))) {
+        Object.assign(ret, typeof arr === "string" ? [arr] : arr);
+      } else {
+        [].push.call(ret, arr);
+      }
+    }
+
+    return ret;
+  }
+
+  /**
+    * @reference underscore.isObject
+  **/
+  isObject(obj) {
+    var type = typeof obj;
+    return type === 'function' || type === 'object' && !!obj;
+  }
+
+  isArray(obj) {
+    return Object.prototype.toString.call(obj) === '[object Array]';
+  }
+
+  // create object if value is not object or array
+  ensureChildIsObject(children) {
+    for (const k in children) {
+      let ty = typeof children[k];
+      
+      if (ty.search(/^string|number|boolean|function$/i) != -1) {
+        const r = {
+          [`${children.length.toString()} items`]: children[k]
+        };
+        children[k] = r;
+      }
+    }
+  }
+
+  // create array from object for each member in an array
+  makeArrayForEach(children) {
+    return children.map(child => {
+      if (this.isObject(child)) {
+        return this.makeArray(child);
+      }
+      return child;
+    });
+  }
+ 
+  // make sure the passed in children property is an array
+  ensureChildIsArray(children) {
+    if (!this.isArray(children)) {
+      return [children];
+    }
+    return children;
+  }
+
+  render() {
+    const { children, parentKey } = this.props;
+    
+    this.ensureChildIsObject(children);
+
+    let header = [];
+    let rows = [];
+    Object.keys(children[0]).forEach(d => header.push(<th key={Math.random()}>{isNaN(d) ? d : 'collection'}</th>));
+    
+    children.forEach((child, i) => {
+      let curRow = [];        
+      for (const key in child) {
+        const obj = child[key];
+        const childKey = `${parentKey}/${i}/${key}`;
+
+        let o = [];
+        if (this.isObject(obj)) {
+          o = this.makeArray(obj);
+        }
+        let newChild = [];
+        if (this.isArray(obj) && obj.length > 0) {
+          // if a collection is a variant, make the object an array (ie. const node = [{ 1, "2", { "variant": 3.0 } }])
+          const modifiedArray = this.makeArrayForEach(obj);          
+          newChild.push(
+            <td key={Math.random()}>
+              <JsonNode path={childKey} children={modifiedArray} />          
+            </td>);
+        } else {
+          if (o.length > 0) {
+            newChild.push(<td key={Math.random()}>
+              <JsonNode path={childKey} children={o} />            
+            </td>);
+          } else {
+            newChild.push(<td key={Math.random()}>{obj.toString()}</td>);
+          }
+        }
+        curRow.push(newChild);
+      }
+      rows.push(<tr key={Math.random()}>{curRow}</tr>);
+    });
+
+    return (
+      <table><thead><tr>{header}</tr></thead>
+        <tbody>{rows}</tbody>
+      </table>
+    )
+  }
+}
